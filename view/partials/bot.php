ab6175e806f3150aa8822c2dfe6f6df6089cf8ae
aui_tuthousend <mraihan286@gmail.com>
2024-07-04 20:16:23 +0700

    feat: partials

---
+++ view/partials/bot.php
@@ -0,0 +1,22 @@
+<footer class="col">
+    <div class="row">
+        <img src="/images/png/logo_yayasan.png" width="58" height="58" >
+        <div class="col hc1">
+            <h2>Yayasan Annafilah</h2>
+            <p>Berdaya Bersama</p>
+        </div>
+    </div>
+
+    <div class="faq col">
+        <h3>FAQ</h3>
+        <div class="info row">
+            <h3>Alamat: <br>Pagesangan Town House, Pagesangan 2A Jambangan Surabaya</h3>
+            <h3>Email: <br>yayasan.annafilah@gmail.com</h3>
+            <h3>Whatsapp: <br>081234344229</h3>
+        </div>
+    </div>
+</footer>
+
+<script src="/script.js"></script>
+</body>
+</html>
---
+++ view/partials/top.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Annafilah</title>
+    <link rel="stylesheet" type="text/css" href="/view/css/home.css">
+    <link rel="stylesheet" type="text/css" href="/view/css/artikel.css">
+    <link rel="stylesheet" type="text/css" href="/view/css/viewartikel.css">
+    <link rel="stylesheet" type="text/css" href="/view/css/profile.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/view/css/template.css">
+</head>
+
+<body>
+<header class="heder">
+    <img src="/images/png/logo_yayasan.png" width="58" height="58" >
+    <div class="col hc1">
+        <h2>Yayasan Annafilah</h2>
+        <p>Berdaya Bersama</p>
+    </div>
+    <div class="link row">
+        <a href=/><h4>Home</h4></a>
+        <a href="/profile"><h4>Tentang Kami</h4></a>
+        <a href="/#Program"><h4>Program</h4></a>
+        <a href="/articles"><h4>Artikel</h4></a>
+        <h4>Gallery</h4>
+        <a href="/#regis"><h4>Jadi Donatur</h4></a>
+    </div>
+</header>
