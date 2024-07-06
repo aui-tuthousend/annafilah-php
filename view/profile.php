2ee6a5378594223ed2ebab5ece2122753733704a
aui_tuthousend <mraihan286@gmail.com>
2024-07-06 21:01:48 +0700

    feat: profile

---
+++ view/profile.php
@@ -0,0 +1,36 @@
+<?php include "../view/partials/top.php"; ?>
+
+<div class="ce1 col-c">
+    <h1>Tentang Kami</h1>
+    <div>
+        <h1>Visi</h1>
+        <h1>“Menjadi Lembaga Terpercaya dalam Membangun dan Memberdayakan Umat”</h1>
+    </div>
+    <div>
+        <h1>Misi</h1>
+        <h1>“Membangun Umat yang Cerdas, Mulia, dan Berdaya Guna”</h1>
+    </div>
+</div>
+
+<div class="ce2">
+    <img src="/images/program/bsis.jpg" alt="" class="img foto">
+</div>
+
+<div class="ce3 col-c">
+    <h1>Sejarah</h1>
+    <h3>------</h3>
+    <p>Berawal dari melihat tumbuh kembang anak yatim dhuafa yang memprihatinkan seorang ibu guru ngaji mengajak teman-teman majelis kajian memberikan kepedulian kepada mereka dengan melakukan pendampingan dan bantuan untuk pendidikan agar menjadi generasi cerdas, tangguh, beriman, dan bertaqwa.</p>
+    <p>Dengan semangat berdakwah terus menerus tidak mengenal lelah melakukan pendampingan, akhirnya mendirikan lembaga untuk menunjukkan keseriusannya kepada masyarakat. Dengan memberi nama lembaga tersebut Ashabul Kahfi.</p>
+    <p>Dalam proses pengurusan legalitas untuk mendapatkan pengesahan dari Menkumham para pendiri merubah nama Ashabul Kahfi menjadi Yayasan Annafilah. Maka pada bulan Maret 2011 Yayasan Annafilah menjadi lembaga sosial dakwah yang resmi.</p>
+    <p>Dari dana zakat infaq sedekah yang dihimpun dari para anggota beberapa majelis kajian, Annafilah mengikuti aturan yang dibuat oleh pemerintah, maka dalam hal ini Annafilah bekerja sama dengan lembaga Amil Zakat Nasional. Annafilah menjadi Mitra lembaga Amil Zakat Nasional Nurul Hayat (MPZ ).</p>
+    <p>Pendampingan anak yatim dan dhuafa dilakukan dengan berbasis keluarga artinya anak tidak dipantikan namun tetap bersama orang tua. Dan pembinaannya bukan saja untuk anak namun juga untuk orang tua. Berikut apa saja yang telah dihasilkan dari pembinaan kepada anak yatim-piatu dan orangtuanya juga kepada masyarakat di lingkungan Annafilah.</p>
+    <p>Ada salah satu anak binaan yang ternyata mampu bersaing untuk mengikuti pendidikan di sekolah yang berkualitas di lembaga pendidikan pondok tahfidz swasta. Menjadi juara pidato bahasa Arab tingkat Jawa Timur ketika kelas 2 SMAIT, dan berhasil menyelesaikan hafalan 30 juz pada waktu kelas 3 SMPIT. Saat ini sudah menikah dan menjadi guru tahfidz.</p>
+    <p>Tahun 2022 Allah mudahkan lagi untuk salah satu binaan kami mengukir prestasi dengan berhasil mendapatkan hafidzah dengan sanad Rasulullah melalui tasmi langsung oleh Ust Mudhowi Ma’arif, Lc. Semua binaan kami adalah penghafal Al-Qur'an, Alhamdulilah.</p>
+    <p>Out Put dari pembinaan bunda yatim yang kami beri nama program Sekolah Bunda yaitu berhasil menjadi guru-guru Al Qur’an. Saat ini Annafilah juga mempunyai lembaga TPQ dan Tahfidz sendiri yang menjadi wadah bagi mereka juga melayani masyarakat</p>
+    <p>Selanjutnya ada program safari dakwah yang ditujukan kepada jamaah subuh mushola dan masjid di seputar kelurahan Pagesangan. Masyarakat antusias sekali dengan adanya program ini. Alhamdulillah dakwah Annafilah sudah menyentuh berbagai elemen masyarakat.</p>
+</div>
+
+
+
+<?php include "../view/partials/bot.php"; ?>
+
